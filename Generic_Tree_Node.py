befae1f9d6f0238b75f571900243049d07a52b61
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-01-23 10:04:07 +0530

    PrintTree recursively`
    take a look on class notes as well

--- Generic_Tree_Node.py
+++ Generic_Tree_Node.py
@@ -4,6 +4,29 @@ class TreeNode:
         self.data = data
         #creating an empty list
         self.children = list()
+def printTree(root):
+    #this is not a base case it is a edge case ,if someone gives none then it is going to be crashed,here our base case is a leaf ,if we reach leaf then it is going to be stopped
+    if root == None:
+        return 
+    print(root.data)
+    for child in root.children:
+        printTree(child)
+
+def printTreeDetail(root):
+    #edge case
+    if root == None:
+        return
+        #this will print roots value first ,and i dont want to print on new line so using end= ""
+    print(root.data, ":" ,end="")
+    #for below call this will print 
+    for child in root.children:
+        print(child.data, ",", end="")
+    #this to get a new line 
+    print()
+        #this will call on each child 
+    for child in root.children:
+        printTreeDetail(child)
+
 n1 = TreeNode(5)
 n2 = TreeNode(2)
 n3 = TreeNode(9)
@@ -18,3 +41,6 @@ n1.children.append(n4)
 n1.children.append(n5)
 n3.children.append(n6)
 n3.children.append(n7)
+
+# printTree(n1)
+printTreeDetail(n1)
