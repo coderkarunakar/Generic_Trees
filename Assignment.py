dd377aee9db66c4d76cb1db89e0d113d7fb2c9d6
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-01-31 09:39:47 +0530

    nodes greater than x

--- Assignment.py
+++ Assignment.py
@@ -49,7 +49,27 @@ def is_present_in_tree(root, x):
 #if  x is not found in the current node or its children then simply return false
     return False
 
+#Nodes greater than y
+def count_nodes_greater_than_y(root, x):
+    if root is None:
+        return 0
 
+    count = 0
+
+    # Iterate through children
+    #this part iterates through each child of the current root node,in treenode class each node has a list of children and this loop going through each child in that list
+    #NOTE:this is checking only for root.children 
+    for child in root.children:
+        # for each child in that loop it makes a recursive call to this function,this call is essential for traveling the tree structure
+        # Count the nodes with values greater than x in the subtree rooted at the current child, and add that count to the total count,with this also count value is getting increased overall
+        count += count_nodes_greater_than_y(child, x)
+#NOTE:this is checking for the main root value
+    # Check the current node,this is for that main root value,with this the count value is getting increase by 1 because of checking the root value
+    if root.value > x:
+        count += 1
+
+    return count
+#please look in class notes for dry run it is simple one
 
 # Sample input
 root = takeTreeInput()
@@ -57,3 +77,7 @@ printTreeDetail(root)
 x = int(input("please enter the value you are searching"))
 print(is_present_in_tree(root, x))  # Output: True
 
+y = int(input("please enter value of y in order to get values greater than y"))
+# Find and print the number of nodes with data greater than x
+result = count_nodes_greater_than_y(root, y)
+print("Number of nodes with data greater than", y, "is:", result)
